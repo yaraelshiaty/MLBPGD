115d72f1a1aceb193d3a6e54892536ac3983c952
Yara Elshiaty <elshiaty@math.uni-heidelberg.de>
2024-05-27 18:40:14 +0200

    added torch compatible armijo linesearch

--- MGTomo/Yfunctions.py
+++ MGTomo/Yfunctions.py
@@ -13,9 +13,33 @@ def kl_distance(x: torch.tensor, proj: mgproj.TomoTorch, b: torch.tensor):
     
     return fx
 
-def SMART(f, x: torch.tensor, tau):
+def SMART(f, x: torch.tensor, tau = 0.2):
     fx = f(x)
+    #x.requires_grad = True
+    #x.retain_grad()
     fx.backward()
     val = myexp(-tau * x.grad)
     
     return val
+
+def armijo_linesearch(f,x: torch.tensor,d: torch.tensor,a=1.,r=0.5,c=1e-4):
+    fx = f(x)
+    fx.backward()
+    dgk = torch.sum(x.grad*d)
+    f_new,_ = fct(x + a * d)
+    x_new = x.copy()
+    
+    assert dgk <= 0, 'd needs to be a descent direction (dkg = %.5e)' % dgk
+    
+    if dgk == 0.:
+        return x
+    
+    while f_new > f + a * c * dgk and x_new > 0 and a > 1e-7:
+        x_new = x + a * d
+        f_new = f(x_new)
+        a *= r
+    
+    if f_new < f :
+        return x_new, a
+    else:
+        return x
