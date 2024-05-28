running MLO function for one iteration of it, fixed KL and SMART implementation,

MGTomo/Yfunctions.py
@@ -4,21 +4,28 @@ import astra
 import MGTomo.tomoprojection as mgproj
 from MGTomo.utils import myexp, mylog, mydiv
 
-def kl_distance(x: torch.tensor, proj: mgproj.TomoTorch, b: torch.tensor):
+def kl_distance_v2(x: torch.tensor, proj: mgproj.TomoTorch, b: torch.tensor):
     ax = proj(x)
-    ab = torch.divide(ax, b)
+    #ab = torch.divide(ax, b)
+    ab = mydiv(ax,b)
     
     erg = ax * mylog(ab) + b - ax
     fx = torch.sum( erg[b > 0.] ) + 0.5*torch.sum(ax[b == 0.]**2)
-    
+    #fx = torch.sum(erg[b > 0.])
     return fx
 
-def SMART(f, x: torch.tensor, tau = 0.2):
+def kl_distance(x: torch.tensor, proj: mgproj.TomoTorch, b: torch.tensor):
+    ax = proj(x)
+    erg = ax * (mylog(ax) - mylog(b)) + b - ax
+    fx = torch.sum(erg)
+    
+    return fx.requires_grad_(True)
+
+def SMART(f, x: torch.tensor, tau):
     fx = f(x)
-    #x.requires_grad = True
-    #x.retain_grad()
-    fx.backward()
-    val = myexp(-tau * x.grad)
+    fx.backward(retain_graph = True)
+    val = x * myexp(-tau * x.grad)
+    #val.requires_grad = True
     
     return val
 
@@ -26,20 +33,21 @@ def armijo_linesearch(f,x: torch.tensor,d: torch.tensor,a=1.,r=0.5,c=1e-4):
     fx = f(x)
     fx.backward()
     dgk = torch.sum(x.grad*d)
-    f_new,_ = fct(x + a * d)
-    x_new = x.copy()
+    f_new = f(x + a * d)
+    x_new = x
     
     assert dgk <= 0, 'd needs to be a descent direction (dkg = %.5e)' % dgk
     
     if dgk == 0.:
         return x
     
-    while f_new > f + a * c * dgk and x_new > 0 and a > 1e-7:
+    while f_new > fx + a * c * dgk and a > 1e-7:
+        #needed added x_new > 0
         x_new = x + a * d
         f_new = f(x_new)
         a *= r
     
-    if f_new < f :
-        return x_new, a
+    if f_new < fx :
+        return x_new
     else:
         return x