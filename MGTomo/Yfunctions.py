3713498f6ef6dbfe34cd9950bdd7341f4244fb65
Yara Elshiaty <elshiaty@math.uni-heidelberg.de>
2024-06-03 11:59:21 +0200

    fixed armijo linesearch >= 0 criterion

--- MGTomo/Yfunctions.py
+++ MGTomo/Yfunctions.py
@@ -32,26 +32,26 @@ def SMART(f, x: torch.tensor, tau):
     
     #assert f(val) < fx, 'SMART iterations do not descend'
     return val
-
-def armijo_linesearch(f,x: torch.tensor,d: torch.tensor,a=1.,r=0.5,c=1e-4):
+    
+def armijo_linesearch(f, x: torch.tensor, d: torch.tensor, a=1., r=0.5, c=1e-4):
     fx = f(x)
     fx.backward()
-    dgk = torch.sum(x.grad*d)
-    #print(dgk)
-    f_new = f(x + a * d)
-    x_new = x
+    dgk = torch.sum(x.grad * d)
     
-    assert dgk <= 0, 'd needs to be a descent direction (dkg = %.5e)' % dgk
+    assert dgk <= 0, 'd needs to be a descent direction (dgk = %.5e)' % dgk
     
     if dgk == 0.:
-        return x
+        return x, 0.
     
-    while f_new > fx + a * c * dgk and a > 1e-7 and x_new.min() >= 0:
+    while True:
         x_new = x + a * d
         f_new = f(x_new)
+        
+        if f_new <= fx + a * c * dgk and x_new.min() >= 0:
+            break
+        
         a *= r
+        if a <= 1e-7:
+            return x, 0.
     
-    if f_new < fx:
-        return x_new
-    else:
-        return x
+    return x_new, a
